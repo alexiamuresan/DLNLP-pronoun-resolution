3aaac674fc2cf1747768e7ab876661e384d39d15
abhinav-neil <abhinav.neil@gmail.com>
2023-09-25 18:47:28 +0000

    create GAP classifier model & trainer (lightning)

---
+++ src/models.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from transformers import AutoModel, AutoTokenizer
+import pytorch_lightning as pl
+import torchmetrics
+
+class GAPCorefClassifier(pl.LightningModule):
+    """
+    Coreference classifier on GAP dataset using a transformer-based model.
+    """
+
+    def __init__(
+        self,                      
+        lm_name: str='roberta-large',
+        num_classes: int=3,
+        lr: float = 5e-4,
+        weight_decay: float = 5e-4,
+        **kwargs,
+    ):
+        """
+        Initialize GAP Coreference Classifier.
+
+        Args:
+            lm_name (str): Pretrained model name or path.
+            num_classes (int): Number of output labels.
+            lr (float): Learning rate for optimizer (Adam).
+            weight_decay (float): Weight decay for optimizer (Adam).
+        """
+        super().__init__()
+        self.save_hyperparameters()
+        
+        # instantiate BERT model
+        self.model = AutoModel.from_pretrained(lm_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(lm_name)
+        # self.dropout = nn.Dropout(0.3)
+        self.fc = nn.Linear(self.model.config.hidden_size, num_classes)
+        
+        # instantiate loss function
+        self.loss = nn.CrossEntropyLoss()
+
+        # instantiate accuracy metric
+        self.acc = torchmetrics.Accuracy(task='multiclass', num_classes=num_classes, average='macro')
+        self.f1 = torchmetrics.F1Score(task='multiclass', num_classes=num_classes, average='macro')
+
+    def forward(self, text, pronoun, a, b):
+        # Combine the text, pronoun, and referents A and B as a single input sequence.
+        inputs = self.tokenizer(
+            [f"{text} [SEP] {pronoun} [SEP] {a} [SEP] {b}" for text, pronoun, a, b in zip(text, pronoun, a, b)],
+            padding=True,
+            truncation=True,
+            max_length=512,
+            return_tensors="pt"
+        )
+        # Move inputs to the same device as the model
+        inputs = {key: val.to(self.device) for key, val in inputs.items()}
+        outputs = self.model(**inputs)
+        pooled_output = outputs.last_hidden_state[:, 0]
+        logits = self.fc(pooled_output)
+        return logits
+    
+    def step(self, batch):
+        text, pronoun, a, b, labels = batch['text'], batch['pronoun'], batch['a'], batch['b'], batch['label']
+        logits = self.forward(text, pronoun, a, b)
+        loss = self.loss(logits, labels)
+        return loss, logits, labels
+
+    def training_step(self, batch, batch_idx):
+        loss, logits, y = self.step(batch)
+        self.log("train_loss", loss, batch_size=len(y), on_epoch=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss, logits, y = self.step(batch)
+        preds = torch.argmax(logits, dim=1)
+        acc = self.acc(preds, y)  # Compute accuracy
+        f1 = self.f1(preds, y)  # Compute f1
+        self.log("val_loss", loss, batch_size=len(y), on_epoch=True)
+        self.log("val_acc", acc, batch_size=len(y), on_epoch=True)  # Log accuracy
+        self.log("val_f1", f1, batch_size=len(y), on_epoch=True)  # Log f1
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        loss, logits, y = self.step(batch)
+        preds = torch.argmax(logits, dim=1)
+        acc = self.acc(preds, y)  # Compute accuracy
+        f1 = self.f1(preds, y)  # Compute f1
+        self.log("test_acc", acc, batch_size=len(y), on_epoch=True)  # Log accuracy
+        self.log("test_f1", f1, batch_size=len(y), on_epoch=True)  # Log f1
+        return loss
+    
+    def predict_step(self, batch, batch_idx):
+        _, logits, labels = self.step(batch)
+        preds = torch.argmax(logits, dim=1)
+        return labels, preds
+    
+    def configure_optimizers(self):
+        optimizer = Adam(
+            self.parameters(),
+            lr=self.hparams.lr,
+            weight_decay=self.hparams.weight_decay
+        )
+        return optimizer
